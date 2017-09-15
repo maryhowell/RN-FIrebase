0c971942486383773b1868aa16d0fd7a32210abc
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-15 13:46:56 -0700

    makeing albumlist a class

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
+class AlbumList extends Component {
   return (
     <View>
       <Text> Album List </Text>
