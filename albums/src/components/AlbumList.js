0661d981e0c8bc4401ffd4a789bc52922201e4a6
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-16 13:49:12 -0700

    refactor

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
-  return (
-    <View>
-      <Text> Album List </Text>
-    </View>
+  render() {
+    return (
+      <View>
+        <Text> Album List </Text>
+      </View>
 
-  );
+    );
+  }
 };
 
 export default AlbumList;
