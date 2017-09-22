53673070a74e308f7db04bf436b6e16106042afe
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-22 14:17:03 -0700

    albums render

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -10,10 +10,14 @@ class AlbumList extends Component {
     .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     return (
       <View>
-        <Text> Album List </Text>
+        {this.renderAlbums()}
       </View>
 
     );
