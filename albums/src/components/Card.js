42098f4401dd6d685db2d4baea1967034eab2969
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-30 20:48:44 -0700

    adding styling to card

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -3,8 +3,25 @@ import { View } from 'react-native';
 
 const Card = () => {
   return (
-    <View></View>
+    <View style={styles.constainerStyle}></View>
   );
+};
+
+const styles = {
+  constainerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor:'#ddd',
+    boderWidthBottom: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRaduis: 2,
+    elevation: 1,
+    marginRight: 5,
+    marginLeft: 5,
+    margintop: 10
+  }
 };
 
 export default Card;
