f10c419cb248d4425437e03b9e16161df1f9036b
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-14 17:54:01 -0800

    adding style

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
+  const { textStyle } = styles;
   return <Text>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 export default Header;
