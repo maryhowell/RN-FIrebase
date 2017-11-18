02d604f64465a4b869e97710d5cb6f4ea4d21cb5
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-17 17:57:53 -0800

    adding background color

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyles } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return  (
+    <View style={viewStyles}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  )
 };
 
 const styles = {
+  viewStyles:{
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
