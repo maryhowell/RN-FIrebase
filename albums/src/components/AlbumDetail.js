56577fae3cbd22982eb9993271dc98d733a10283
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-19 20:48:11 -0800

    adding view to album details

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection';
 
@@ -7,6 +7,7 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
+        <View></View>
         <Text>{props.album.title}</Text>
       </CardSection>  
     </Card>
