2e3b2f360a863e9ca4f25fbfeb66b1d5fe0c421a
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-21 16:26:12 -0800

    adding new files

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -14,6 +14,7 @@ class AlbumList extends Component {
   renderAlbums() {
     return this.state.albums.map(album =>
       <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
