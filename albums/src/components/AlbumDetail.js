5bcec2809ffeb46cf2271da9e8bbbd3375e1ee29
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-29 13:19:06 -0800

    styling for thumbnail container

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -33,6 +33,12 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
   
 };
