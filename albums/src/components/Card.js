b7481d3dcb301a886cdc48126cf7b84309edebd2
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-28 19:08:54 -0800

    refactor

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -14,7 +14,7 @@ const styles = {
     borderWidth: 1,
     borderRadius: 2,
     borderColor:'#ddd',
-    boderWidthBottom: 0,
+    borderWidthBottom: 0,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
