5c6d175c9ac0c344ae3cc5fe9d342cecf3d58a3a
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-29 13:27:39 -0800

    adding album images

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,11 +4,13 @@ import Card from './Card'
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const { 
-    headerContentStyle, 
-    thumbnailStyle,
-    thumbnailContainerStyle  
+      headerContentStyle, 
+      thumbnailStyle,
+      thumbnailContainerStyle,
+      headerTextStyle,
+      imageStyle  
     } = styles;
   
   return (
@@ -21,9 +23,16 @@ const AlbumDetail = ({ album }) => {
           />
         </View>
         <View style={ headerContentStyle }>
-          <Text>{ title }</Text>
+          <Text style={ headerTextStyle }>{ title }</Text>
           <Text>{ artist }</Text>
         </View>  
+      </CardSection> 
+       
+      <CardSection>
+        <Image 
+          style={ imageStyle }
+          source={{ uri: image }}
+         />
       </CardSection>  
     </Card>
   );
@@ -34,6 +43,9 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
@@ -43,6 +55,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
   
 };
