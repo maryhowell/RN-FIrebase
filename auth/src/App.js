bd014e261cbc4c3d1eeece8729c361537dedd485
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-29 19:06:18 -0800

    adding LogIn Form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
+
 
 class App extends Component {
+
+
   render() {
     return (
       <View>
       <Header headerText='Authentication' />
-        <Text> An App </Text>
+        <LoginForm />
       </View>
     );
   }
