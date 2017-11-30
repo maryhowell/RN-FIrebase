71b3c100f8c4c53df5dea8788f34024f4ebf99cf
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-11-30 15:55:05 -0800

    adding spinner

--- auth/src/App.js
+++ auth/src/App.js
@@ -6,6 +6,17 @@ import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+  
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCxaRexqVV6OCLFDSmbNYr4ex5EVentwuc',
+      authDomain: 'auth-eb26f.firebaseapp.com',
+      databaseURL: 'https://auth-eb26f.firebaseio.com',
+      projectId: 'auth-eb26f',
+      storageBucket: 'auth-eb26f.appspot.com',
+      messagingSenderId: '57614860138'
+    });
+  }
 
 
   render() {
