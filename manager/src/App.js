3fae5e863a223b98d118b49082545eb0fa89ab0e
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-12-04 12:33:13 -0800

    adding project about promises

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
+
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyC8gCuI2BqtUNT4CeCLeGx31yY6R2KKvns',
+      authDomain: 'managers-d61c1.firebaseapp.com',
+      databaseURL: 'https://managers-d61c1.firebaseio.com',
+      projectId: 'managers-d61c1',
+      storageBucket: 'managers-d61c1.appspot.com',
+      messagingSenderId: '73904527882'
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <Provider store={ createStore(reducers) }>
