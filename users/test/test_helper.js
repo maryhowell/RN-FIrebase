fb7f4347ae5ec9ac7a1a7530070ac452642ebb83
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-12-04 14:11:46 -0800

    refactor

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/users_test)');
-mongoode.connection
+mongoose.connection
   .once('open', () => console.log('Good to go');)
   .on('error',(error) => {
     console.warn('Warning', error);
