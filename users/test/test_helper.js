3958d0d522c0f78910320deebfa5086fb7889fce
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-12-06 11:43:40 -0800

    adding testing project

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/users_test)');
 mongoose.connection
-  .once('open', () => console.log('Good to go');)
+  .once('open', () => console.log('Good to go'))
   .on('error',(error) => {
     console.warn('Warning', error);
   });
